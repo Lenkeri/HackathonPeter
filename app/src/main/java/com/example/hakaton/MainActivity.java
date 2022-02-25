35c6665fc5ccb8347f2ab3e74b83c3691d09d80a
Lenkeri <lno4kamalt@yandex.ru>
2022-02-25 15:05:33 +0500

    ab

--- app/src/main/java/com/example/hakaton/MainActivity.java
+++ app/src/main/java/com/example/hakaton/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.hakaton.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
     }
+
+    public static void main(String[] args) {
+
+    }
+
+
 }
