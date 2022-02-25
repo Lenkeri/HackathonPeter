9818b6ddde2d491cef9601e8186950e42aa51b80
Lenkeri <lno4kamalt@yandex.ru>
2022-02-25 15:58:59 +0500

    ab

--- app/src/main/java/com/example/hakaton/MainActivity.java
+++ app/src/main/java/com/example/hakaton/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.hakaton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.hakaton.databinding.ActivityMainBinding;
 
@@ -16,10 +17,33 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
         setContentView(binding.getRoot());
+        binding.calculateButton.setOnClickListener(view -> setAnswer());
     }
 
-    public static void main(String[] args) {
+    private void setAnswer() {
+        String a = binding.textAField.getEditText().getText().toString();
+        String b = binding.textBField.getEditText().getText().toString();
+        String c = binding.textCField.getEditText().getText().toString();
 
+        if ("".equals(a) || "".equals(b) || "".equals(c)) {
+            return;
+        }
+
+        Double aField = Double.valueOf(a);
+        Double bField = Double.valueOf(b);
+        Double cField = Double.valueOf(c);
+
+        Double D = bField * bField - 4 * aField * cField;
+
+        if(D < 0){
+            return;
+        }
+        int x1 = (int)((- bField + Math.sqrt(D) ) / 2 * aField);
+        int x2 =  (int)((- bField - Math.sqrt(D) ) / 2 * aField);
+
+
+        binding.x1TextView.setText("x1:" + x1);
+        binding.x2TextView.setText("x2:" + x2);
     }
 
 
